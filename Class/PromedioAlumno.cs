a8b61cc624ecb2120830f32d384c92a44b94145a
guill <guill@GUILLERMO-PC>
2024-05-02 22:23:18 -0400

    Add project files.

---
+++ Class/PromedioAlumno.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace programa3._2.Class
+{
+    using System;
+
+    class PromedioAlumno
+    {
+        public static bool EsPromocionado(double nota1, double nota2, double nota3)
+        {
+            double promedio = (nota1 + nota2 + nota3) / 3.0;
+            return promedio >= 7.0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using programa3._2.Class;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double nota1, nota2, nota3;
+        bool inputCorrecto = false;
+
+        do
+        {
+            Console.WriteLine("Ingrese la primera nota:");
+            if (!double.TryParse(Console.ReadLine(), out nota1))
+            {
+                Console.WriteLine("Error: Por favor ingrese un número válido.");
+            }
+            else
+            {
+                inputCorrecto = true;
+            }
+        } while (!inputCorrecto);
+
+        inputCorrecto = false;
+
+        do
+        {
+            Console.WriteLine("Ingrese la segunda nota:");
+            if (!double.TryParse(Console.ReadLine(), out nota2))
+            {
+                Console.WriteLine("Error: Por favor ingrese un número válido.");
+            }
+            else
+            {
+                inputCorrecto = true;
+            }
+        } while (!inputCorrecto);
+
+        inputCorrecto = false;
+
+        do
+        {
+            Console.WriteLine("Ingrese la tercera nota:");
+            if (!double.TryParse(Console.ReadLine(), out nota3))
+            {
+                Console.WriteLine("Error: Por favor ingrese un número válido.");
+            }
+            else
+            {
+                inputCorrecto = true;
+            }
+        } while (!inputCorrecto);
+
+        if (PromedioAlumno.EsPromocionado(nota1, nota2, nota3))
+        {
+            Console.WriteLine("Promocionado");
+        }
+
+        Console.ReadLine();
+    }
+}
